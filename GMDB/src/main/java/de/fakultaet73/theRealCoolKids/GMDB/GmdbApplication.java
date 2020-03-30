b1aa9ee43943f96d240bcd9c5e4d180819dca5bb
Halil <h.i.tuerk@googlemail.com>
2020-03-30 10:09:08 +0200

    init backend

---
+++ GMDB/src/main/java/de/fakultaet73/theRealCoolKids/GMDB/GmdbApplication.java
@@ -0,0 +1,13 @@
+package de.fakultaet73.theRealCoolKids.GMDB;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GmdbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GmdbApplication.class, args);
+	}
+
+}
---
+++ GMDB/src/test/java/de/fakultaet73/theRealCoolKids/GMDB/GmdbApplicationTests.java
@@ -0,0 +1,13 @@
+package de.fakultaet73.theRealCoolKids.GMDB;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GmdbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
